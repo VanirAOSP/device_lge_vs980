1ac48edd943d8ac319a43fe306e32ff5e534e23c
sudosurootdev <sudosurootdev@gmail.com>
2015-01-08 23:07:11 -0500

    Add the only other changes, which could be causing VZW G2 roaming.
    Change-Id: Id077a64408aa7ab485419011e2b30bce85f6d077

--- device.mk
+++ device.mk
@@ -22,10 +22,11 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	telephony.lteOnCdmaDevice=1 \
+        ro.telephony.default_cdma_sub=0 \
 	ro.telephony.default_network=10 \
 	ro.cdma.home.operator.numeric=310004 \
 	ro.cdma.home.operator.alpha=Verizon \
-	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83
+	ro.cdma.homesystem=66,67,69,71,72,74
 
 # GPS configuration
 PRODUCT_COPY_FILES += \
